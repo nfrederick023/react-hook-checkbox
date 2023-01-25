e2895ed72eee4505505717b0a996412651dba540
nfrederick023 <nfrederick023@gmail.com>
2023-01-24 18:44:17 -0600

    Remove Immer dep.

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,62 @@
-import { immerable } from "immer";
+declare class SelectUtility<P> {
+    public readonly sections: Section<P>[];
 
-export function useCheckbox<P = Record<string, never>>(items: SelectUtilityBase<P>[]): [SelectUtility<P>];
+    constructor(sections: Section<P>[]);
 
-export enum Action {
+    public setSections(sections: Section<P>[]): void;
+    public createNewUtility(items: SelectUtilityBase<P>[]): void;
+    public isAnyItemChecked(): boolean;
+    public addSection(sectionName: string): void;
+    public getSelectedSections(): Section<P>[];
+}
+
+declare class Section<P> {
+    public readonly name: string;
+    public readonly items: Item<P>[];
+    public readonly id: number;
+
+    constructor(name: string, items: Item<P>[], id: number);
+
+    public setSectionName(name: string): void;
+    public setItems(items: Item<P>[]): void;
+    public selectAll(): void;
+    public removeSection(): void;
+    public addItem(item: ItemBase<P>): void;
+    public isIndeterminate(): boolean;
+    public isAllSelected(): boolean;
+    public isAnySelected(): boolean;
+    public getSelectedItems(): Item<P>[];
+}
+
+declare class Item<P> {
+    public readonly name: string;
+    public readonly isSelected: boolean;
+    public readonly properties: P;
+    public readonly id: number;
+
+    constructor(name: string, isSelected: boolean, properties: P, id: number);
+
+    public select(): void;
+    public setProperties(properties: P): void;
+    public setItemName(name: string): void;
+    public setSelected(selected: boolean): void;
+    public removeItem(): void;
+}
+
+declare interface SelectUtilityBase<P> {
+    itemName: string;
+    sectionName?: string;
+    isSelected?: boolean;
+    properties?: P;
+}
+
+declare interface ItemBase<P> {
+    itemName: string;
+    isSelected?: boolean;
+    properties?: P;
+}
+
+declare enum Action {
     Select = 0,
     SetProperties = 1,
     SetItemName = 2,
@@ -11,110 +65,62 @@ export enum Action {
     SetItems = 5,
     SelectAll = 6,
     SetSections = 7,
-    CreateNewUtility = 8
+    CreateNewUtility = 8,
+    RemoveItem = 9,
+    RemoveSection = 10
+
 }
 
-export class SelectUtility<P> {
-    [immerable]: boolean;
-    public readonly sections: Section<P>[];
-
-    constructor(sections: Section<P>[]);
-
-    public setSections(sections: Section<P>[]): void;
-    public createNewUtility(items: SelectUtilityBase<P>[]): void;
-    public isAnyItemChecked(): boolean;
-    public getSelectedSections(): Section<P>[];
+declare interface SetSectionBase {
+    readonly sectionID: number;
 }
 
-export class Section<P> {
-    [immerable]: boolean;
-    public readonly name: string;
-    public readonly items: Item<P>[];
-
-    constructor(name: string, items: Item<P>[]);
-
-    public setSectionName(name: string): void;
-    public setItems(items: Item<P>[]): void;
-    public selectAll(): void;
-    public isIndeterminate(): boolean;
-    public isAllSelected(): boolean;
-    public isAnySelected(): boolean;
-    public getSelectedItems(): Item<P>[];
+declare interface SetItemBase {
+    readonly itemID: number;
 }
 
-export class Item<P> {
-    [immerable]: boolean;
-    public readonly name: string;
-    public readonly sectionName: string;
-    public readonly isSelected: boolean;
-    public readonly properties: P;
-
-    constructor(name: string, sectionName: string, isSelected: boolean, properties: P);
-
-    public select(): void;
-
-    public setProperties(properties: P): void;
-
-    public setItemName(name: string): void;
-
-    public setSelected(selected: boolean): void;
-}
-
-export interface SelectUtilityBase<P> {
-    itemName: string;
-    sectionName?: string;
-    isSelected?: boolean;
-    properties?: P;
-}
-
-export interface SetSectionBase<P> {
-    readonly section: Section<P>;
-}
-
-export interface SetItemBase<P> {
-    readonly item: Item<P>;
-}
-
-export interface SetProperties<P> extends SetItemBase<P> {
+declare interface SetProperties<P> extends SetItemBase {
     readonly properties: P;
 }
 
-export interface SetItemName<P> extends SetItemBase<P> {
+declare interface SetItemName extends SetItemBase {
     readonly name: string;
 }
 
-export interface SetSelected<P> extends SetItemBase<P> {
+declare interface SetSelected extends SetItemBase {
     readonly isSelected: boolean;
 }
 
-export interface SetSectionName<P> extends SetSectionBase<P> {
+declare interface SetSectionName extends SetSectionBase {
     readonly name: string;
 }
 
-export interface SetItems<P> extends SetSectionBase<P> {
+declare interface SetItems<P> extends SetSectionBase {
     readonly items: Item<P>[];
 }
 
-export interface SetSections<P> {
+declare interface SetSections<P> {
     sections: Section<P>[];
 }
 
-export interface CreateNewUtility<P> {
+declare interface CreateNewUtility<P> {
     items: SelectUtilityBase<P>[];
 }
 
-export interface ActionType<T extends Action, P> {
+declare interface ActionType<T extends Action, P> {
     type: T,
     params:
-    T extends Action.Select ? SetItemBase<P> :
+    T extends Action.Select ? SetItemBase :
     T extends Action.SetProperties ? SetProperties<P> :
-    T extends Action.SetItemName ? SetItemName<P> :
-    T extends Action.SetSelected ? SetSelected<P> :
-    T extends Action.SetSectionName ? SetSectionName<P> :
+    T extends Action.SetItemName ? SetItemName :
+    T extends Action.SetSelected ? SetSelected :
+    T extends Action.SetSectionName ? SetSectionName :
     T extends Action.SetItems ? SetItems<P> :
-    T extends Action.SelectAll ? SetSectionBase<P> :
+    T extends Action.RemoveItem ? SetItemBase :
+    T extends Action.SelectAll ? SetSectionBase :
     T extends Action.SetSections ? SetSections<P> :
+    T extends Action.RemoveSection ? SetSectionBase :
     T extends Action.CreateNewUtility ? CreateNewUtility<P> : never;
 }
 
-export type ActionStfy<T extends Action, P> = Record<keyof ActionType<T, P>, ActionType<T, P>[keyof ActionType<T, P>]>;
+declare type ActionStfy<T extends Action, P> = Record<keyof ActionType<T, P>, ActionType<T, P>[keyof ActionType<T, P>]>;
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const immer = require("immer");
-const useImmer = require("use-immer");
+import { useReducer } from 'react';
 
 const actions = ["Select", "SetProperties", "SetItemName", "SetSelected", "SetSectionName", "SetItems", "SelectAll", "SetSections", "CreateNewUtility"];
 
@@ -10,9 +9,20 @@ let Action;
     })
 };
 
-exports.useCheckbox = (items) => {
+const genID = () => {
+    const str = (Math.random() * 10000).toString();
+    let hash = 0;
+    for (let i = 0, len = str.length; i < len; i++) {
+        const chr = str.charCodeAt(i);
+        hash = (hash << 5) - hash + chr;
+        hash |= 0;
+        hash = hash < 1 ? hash * -1 : hash;
+    }
+    return hash;
+};
+
+export const useCheckbox = (items) => {
     class CheckboxHook {
-        [immer.immerable] = true;
         sections;
 
         constructor(sections) {
@@ -33,6 +43,14 @@ exports.useCheckbox = (items) => {
             });
         }
 
+        addSection(sectionName) {
+            const type = Action.SetSections;
+            const sections = [...this.sections, new Section(sectionName, [], genID())];
+            setState({
+                type, params: { sections }
+            });
+        }
+
         isAnyItemChecked() {
             return !!this.sections.find(section => section.isAnySelected());
         }
@@ -43,33 +61,51 @@ exports.useCheckbox = (items) => {
     }
 
     class Section {
-        [immer.immerable] = true;
         name;
         items;
+        id;
 
-        constructor(name, items) {
+        constructor(name, items, id) {
             this.name = name;
             this.items = items;
+            this.id = id;
         }
 
         setSectionName(name) {
             const type = Action.SetSectionName;
             setState({
-                type, params: { name, section: this }
+                type, params: { name, sectionID: this.id }
             });
         }
 
         setItems(items) {
             const type = Action.SetItems;
             setState({
-                type, params: { items, section: this }
+                type, params: { items, sectionID: this.id }
             });
         }
 
         selectAll() {
             const type = Action.SelectAll;
             setState({
-                type, params: { section: this }
+                type, params: { sectionID: this.id }
+            });
+        }
+
+        removeSection() {
+            const type = Action.SetSections;
+            setState({
+                type, params: { sectionID: this.id }
+            });
+        }
+
+        addItem(item) {
+            item.isSelected ??= false;
+            item.properties ??= {};
+            const items = [...this.items, new Item(item.itemName, item.isSelected, item.properties, genID())];
+            const type = Action.SetItems;
+            setState({
+                type, params: { items, sectionID: this.id }
             });
         }
 
@@ -94,150 +130,251 @@ exports.useCheckbox = (items) => {
     }
 
     class Item {
-        [immer.immerable] = true;
         name;
         isSelected;
-        sectionName;
         properties;
+        id;
 
-        constructor(name, sectionName, isSelected, properties) {
+        constructor(name, isSelected, properties, id) {
             this.name = name;
-            this.sectionName = sectionName;
             this.isSelected = isSelected;
             this.properties = properties;
+            this.id = id;
         }
 
         select() {
             const type = Action.Select;
             setState({
-                type, params: { item: this }
+                type, params: { itemID: this.id }
             });
         }
 
         setProperties(properties) {
             const type = Action.SetProperties;
             setState({
-                type, params: { properties, item: this }
+                type, params: { properties, itemID: this.id }
             });
         }
 
         setItemName(name) {
             const type = Action.SetItemName;
             setState({
-                type, params: { name, item: this }
+                type, params: { name, itemID: this.id }
             });
         }
 
         setSelected(isSelected) {
             const type = Action.SetSelected;
             setState({
-                type, params: { isSelected, item: this }
+                type, params: { isSelected, itemID: this.id }
+            });
+        }
+
+        removeItem() {
+            const type = Action.RemoveItem;
+            setState({
+                type, params: { itemID: this.id }
             });
         }
     }
 
-    const checkboxHookReducer = (draft, action) => {
-        const getItem = (sectionName, itemName) => getSection(sectionName)?.items.find(item => item.name === itemName);
-        const getSection = (sectionName) => draft.sections.find((section) => section.name === sectionName);
+    const checkboxHookReducer = (state, action) => {
+
+        const updateSection = (newSection) => {
+            const oldSectionIndex = state.sections.findIndex(section => section.id === newSection.id);
+            const newSections = [...state.sections];
+            newSections[oldSectionIndex] = newSection;
+            return new CheckboxHook(newSections);
+        };
+
+        const updateItem = (newItem) => {
+            const section = getSectionWithItem(newItem.id);
+            const oldItemIndex = section?.items.findIndex(item => item.id === newItem.id) ?? 0;
+            const newItems = section?.items ? [...section.items] : [];
+            newItems[oldItemIndex] = newItem;
+
+            if (newItems && section)
+                return updateSection(new Section(section.name, newItems, section.id));
+        };
+
+        const getSectionWithItem = (itemID) => {
+            return state.sections.find(section => section.items.find(item => item.id === itemID));
+        };
+
+        const getItem = (itemID) => {
+            for (let i = 0; i < state.sections.length; i++) {
+                const item = state.sections[i].items.find(item => item.id === itemID);
+                if (item)
+                    return item;
+            }
+        };
+
+        const getSection = (sectionID) => {
+            return state.sections.find(section => section.id === sectionID);
+        };
+
         switch (action.type) {
             case Action.Select: {
-                const item = getItem(action.params.item.sectionName, action.params.item.name);
-                if (item) {
-                    item.isSelected = !item.isSelected;
-                }
+                const item = getItem(action.params.itemID);
+                if (!item)
+                    break;
+
+                const updatedHook = updateItem(new Item(item.name, !item.isSelected, item.properties, item.id));
+                if (updatedHook)
+                    return updatedHook;
+
                 break;
             }
+
             case Action.SetProperties: {
-                const item = getItem(action.params.item.sectionName, action.params.item.name);
-                if (item)
-                    item.properties = action.params.properties;
+                const item = getItem(action.params.itemID);
+                if (!item)
+                    break;
+
+                const updatedHook = updateItem(new Item(item.name, item.isSelected, action.params.properties, item.id));
+                if (updatedHook)
+                    return updatedHook;
+
                 break;
             }
+
             case Action.SetItemName: {
-                const item = getItem(action.params.item.sectionName, action.params.item.name);
-                if (item)
-                    item.name = action.params.name;
+                const item = getItem(action.params.itemID);
+                if (!item)
+                    break;
+
+                const updatedHook = updateItem(new Item(action.params.name, item.isSelected, item.properties, item.id));
+                if (updatedHook)
+                    return updatedHook;
+
                 break;
             }
+
             case Action.SetSelected: {
-                const item = getItem(action.params.item.sectionName, action.params.item.name);
-                if (item)
-                    item.isSelected = action.params.isSelected;
+                const item = getItem(action.params.itemID);
+                if (!item)
+                    break;
+
+                const updatedHook = updateItem(new Item(item.name, action.params.isSelected, item.properties, item.id));
+                if (updatedHook)
+                    return updatedHook;
+
                 break;
             }
+
+            case Action.RemoveItem: {
+                const section = getSectionWithItem(action.params.itemID);
+                if (!section)
+                    break;
+
+                const newItems = section.items.filter(item => item.id !== action.params.itemID);
+                const updatedHook = updateSection(new Section(section.name, newItems, section.id));
+                if (updatedHook)
+                    return updatedHook;
+
+                break;
+            }
+
             case Action.SetSectionName: {
-                const section = getSection(action.params.section.name);
-                if (section)
-                    section.name = action.params.name;
+                const section = getSection(action.params.sectionID);
+                if (!section)
+                    break;
+
+                const updatedHook = updateSection(new Section(action.params.name, section.items, section.id));
+                if (updatedHook)
+                    return updatedHook;
+
                 break;
             }
+
             case Action.SetItems: {
-                const section = getSection(action.params.section.name);
-                if (section)
-                    section.items = action.params.items;
+                const section = getSection(action.params.sectionID);
+                if (!section)
+                    break;
+
+                const updatedHook = updateSection(new Section(section.name, action.params.items, section.id));
+                if (updatedHook)
+                    return updatedHook;
+
                 break;
             }
+
             case Action.SelectAll: {
-                const section = getSection(action.params.section.name);
-                if (section) {
-                    const isIndeterminate = section.isIndeterminate();
-                    section.items.forEach((item) => {
-                        if (isIndeterminate)
-                            item.isSelected = true;
-                        else
-                            item.isSelected = !item.isSelected;
-                    });
-                }
+                const section = getSection(action.params.sectionID);
+
+                if (!section)
+                    break;
+
+                const newItems = section.items.map(item => {
+                    let selectAllState = false;
+                    if (section.isIndeterminate())
+                        selectAllState = true;
+                    else
+                        selectAllState = !item.isSelected;
+                    return new Item(item.name, selectAllState, item.properties, item.id);
+                });
+
+                const updatedHook = updateSection(new Section(section.name, newItems, section.id));
+                if (updatedHook)
+                    return updatedHook;
+
                 break;
             }
+
+            case Action.RemoveSection: {
+                const newSections = state.sections.filter(section => section.id !== action.params.sectionID);
+                return new CheckboxHook(newSections);
+            }
+
             case Action.SetSections: {
-                draft.sections = action.params.sections;
-                break;
+                return new CheckboxHook(action.params.sections);
             }
+
             case Action.CreateNewUtility: {
                 const newUtility = intializeUtility(action.params.items);
-                draft.sections = newUtility.sections;
-                break;
+                return new CheckboxHook(newUtility.sections);
             }
-            default:
-                break;
         }
+        throw Error('Unknown action.');
     };
+
     const intializeUtility = (items) => {
+
         const sections = [];
         items.forEach((newItem) => {
 
-            // default values
             newItem.isSelected ??= false;
             newItem.properties ??= {};
             newItem.sectionName ??= 'unnamed_section';
+            const itemID = genID();
 
-            const sectionInNewState = sections.find((section) => section.name === newItem.sectionName);
+            const sectionInNewState = sections.find(section => section.name === newItem.sectionName);
             let itemInOldState = undefined;
             try {
                 itemInOldState = state.sections.find(section => section.name === newItem.sectionName)?.items.find(item => item.name === newItem.itemName);
-            }
-            catch (e) { /* empty */ }
-            // add new section and new item
+            } catch (e) { /* empty */ }
+
             if (!sectionInNewState) {
-                sections.push(new Section(newItem.sectionName, [new Item(newItem.itemName, newItem.sectionName, newItem.isSelected, newItem.properties)]));
+                const sectionID = genID();
+                sections.push(new Section(newItem.sectionName, [new Item(newItem.itemName, newItem.isSelected, newItem.properties, itemID)], sectionID));
                 return;
             }
-            // add exsisting item to exsisting section
+
             if (itemInOldState) {
                 sectionInNewState.items.push(itemInOldState);
                 return;
             }
-            // add new item to exsisting state
+
             if (!itemInOldState) {
-                sectionInNewState.items.push(new Item(newItem.itemName, newItem.sectionName, newItem.isSelected, newItem.properties));
+                sectionInNewState.items.push(new Item(newItem.itemName, newItem.isSelected, newItem.properties, itemID));
                 return;
             }
+
         });
+
         return new CheckboxHook(sections);
     };
 
-    // state
-    const [state, setState] = useImmer.useImmerReducer((draft, action) => { checkboxHookReducer(draft, action); }, intializeUtility(items));
+    const [state, setState] = useReducer(checkboxHookReducer, intializeUtility(items));
     return [state];
 };
