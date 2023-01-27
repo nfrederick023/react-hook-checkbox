12826d9936c2f76669e46a45c337f40ee20cc471
nfrederick023 <nfrederick023@gmail.com>
2023-01-27 16:56:38 -0600

    Add Jest Testomg

--- index.spec.ts
+++ index.spec.ts
@@ -1,5 +1,13 @@
+import { CheckboxConfig, useCheckbox } from '.';
+import { renderHook } from "@testing-library/react-hooks/server";
 
+const config: CheckboxConfig = {
+    name: 'Option 4',
+};
 
-test('test one', () => {
-
+describe('useCheckbox', () => {
+    it('select option', () => {
+        const { result } = renderHook(() => useCheckbox(config));
+        expect(result.current[0].name).toBe('Option 4');
+    });
 });
